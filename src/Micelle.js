62a07f64c1dbb1573727fbf1404b2735cebfbe19
DMP92 <devin.pfromm.20@gmail.com>
2023-01-12 14:38:58 -0500

    smoothed micelle animation

--- src/Micelle.js
+++ src/Micelle.js
@@ -46,14 +46,14 @@ export default function Micelle({ mouse })
             group.current.rotation.y = - THREE.MathUtils.damp(right.current.rotation.y, scroll.offset * 7, 0.1, delta)
             group.current.position.z = THREE.MathUtils.damp(group.current.position.z, scroll.offset * 1.5, 5, delta)
 
-            right.current.rotation.y = THREE.MathUtils.damp(right.current.rotation.y, scroll.offset * 0.85, 4, delta)
-            right.current.position.x =  THREE.MathUtils.damp(right.current.position.x, scroll.offset * 1, 4, delta)
+            right.current.rotation.y = THREE.MathUtils.damp(right.current.rotation.y, scroll.offset * 1.2, 4, delta)
+            right.current.position.x =  THREE.MathUtils.damp(right.current.position.x, scroll.offset * 0.5, 4, delta)
 
             left.current.rotation.y = THREE.MathUtils.damp(left.current.rotation.y, - scroll.offset * 0.85, 4, delta)
             left.current.position.x = THREE.MathUtils.damp(left.current.position.x, - scroll.offset * 1, 4, delta)
             // left.current.position.z = THREE.MathUtils.damp(left.current.position.z, - scroll.offset * 2, 4, delta)
 
-            contaminant.current.position.z = THREE.MathUtils.damp(contaminant.current.position.z, scroll.offset * 1.2, 5, delta)
+            contaminant.current.position.z = THREE.MathUtils.damp(contaminant.current.position.z, scroll.offset * 0.85, 5, delta)
         }
 
         // if (scroll.offset >= 0.5) group.current.position.z = THREE.MathUtils.damp(group.current.position.x, scroll.offset * 50, 2, delta)
